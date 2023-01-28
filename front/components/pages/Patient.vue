6e47f3f380734ced2e48c449c90e709056ab2b24
Julia <julia@create.cl>
2023-01-28 10:12:37 -0300

    gender icon magnification

--- front/components/pages/Patient.vue
+++ front/components/pages/Patient.vue
@@ -10,20 +10,20 @@
                   <v-row class="ma-1 pa-3">
                     <v-col cols="3" class="pa-0 ma-0 text-center" >
                       <v-card height="100%" class="ma-0 pa-0 text-center" elevation="0">
-                        <v-icon large="true" v-if="patient.gender == 'female'" class="vertical-center" x-large>mdi-human-female</v-icon>
-                        <v-icon large="true" v-else class="vertical-center" size="large">mdi-human-male</v-icon>
+                        <v-icon v-if="patient.gender == 'female'" class="vertical-center" size="70px">mdi-human-female</v-icon>
+                        <v-icon v-else class="vertical-center" size="70px">mdi-human-male</v-icon>
                       </v-card>
                     </v-col>
                     <v-col cols="3" class="pa-0 ma-0 text-center"></v-col>
                     <v-col cols="6" class="pa-0 ma-0 text-center">
-                      <v-card  class="ma-1 pa-0" elevation="0">
+                      <v-card  class="mb-1 pa-0" elevation="0">
                         <v-row class="pa-0 ma-0">
                           <v-card-title class="ma-0 pa-2 justify-center">
                             {{patient.name}}, {{patient.lastName}}
                           </v-card-title>
                         </v-row>
                       </v-card>
-                      <v-card class="ma-1 pa-0" elevation="0">
+                      <v-card class="mt-1 pa-0" elevation="0">
                         <v-row class="ma-0 pa-0">
                           <v-card-text class="ma-0 pa-2">
                             <v-row class="pa-2">
