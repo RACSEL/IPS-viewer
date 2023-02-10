a11e54fe950feff33398b8d28b85568a02632e5b
Julia <julia@create.cl>
2023-02-09 22:31:48 -0300

    new form to check fields with fields inside

--- front/components/pages/UploadIPS.vue
+++ front/components/pages/UploadIPS.vue
@@ -12,34 +12,18 @@
         <v-card-text class="pa-0 py-3 pb-4" >
             <v-btn width="140px" color="primary" 
             v-bind="attrs"
-            v-on="on" @click="validateIPS2()">Ver IPS</v-btn>
+            v-on="on" @click="validateIPS()">Ver IPS</v-btn>
         </v-card-text>
         <v-card-text class="pa-0 pt-4 pb-3">
             <v-btn width="140px" color="error" @click="ips=''">Borrar</v-btn>
         </v-card-text>
       </v-col>
     </v-row>
-    <v-row align="center"
-      justify="center">
-      <v-card elevation="0" v-if="false">
-        <v-card-title>
-          Los siguientes elementos no existen o no corresponden al formato:
-        </v-card-title>
-        <v-card-text >
-          <v-list-item v-for="error in errors" :key="error">
-            <v-list-item-content>
-              <v-list-item-title>- {{error}} </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card-text>
-      </v-card>
-    </v-row>
     <div class="text-center">
       <v-dialog
         v-model="dialogErrors"
         width="500"
       >
-
         <v-card>
           <v-card-title class="text-h5 grey lighten-2">
             Problemas con IPS
@@ -85,8 +69,6 @@
       return{
         sampleJson: sample,
         ips: "",
-        errors: [],
-        formats: [],
         warnings: [],
         cardErrors: [],
         formatErrors: [],
@@ -106,7 +88,7 @@
       isObject(myObj){
         return myObj.constructor === Object;
       },
-      validateIPS2(){
+      validateIPS(){
         this.cardErrors = [];
         this.formatErrors = [];
         // 0: 1..1    //1: 1..*    //2: 0..1   //3: 0..*
@@ -135,26 +117,8 @@
           this.sectionFormat = true;
           return;
         }
-        for( let k in fields){ //value: fields[k] type: array
-          let val = fields[k];
-          let card = val[0];
-          let v = ips[k]; //
-          if( v == undefined && (card == 0 || card == 1) ){ //no se encontró y era obligatorio:
-            this.cardErrors.push(k);
-          }
-          else{ // todos los otros q se encontraron:
-            if (val.length > 1){ //tenemos el tipo de dato.
-              let dataType = val[1];
-              // 0: obj,  1: array
-              if( (dataType == 0 && ! this.isObject(v)) || (dataType == 1 && ! this.isArray(v)) ){ //si debiese ser un obj y no es un obj... error de formato // lo mismo con array
-                this.formatErrors.push(k);
-              }
-              if (dataType == 1 && card == 1 && v.length<1){ //requiere ser más de 1 y el tipo de dato es array
-                this.cardErrors.push("slices en entry");
-              }
-            }
-          }
-
+        //this.validateCardAndFormat(fields, ips);
+        this.validateComposition(ips);
         if(this.cardErrors.length > 0){
           this.dialogErrors = true;
           this.sectionErrors = true;
@@ -163,73 +127,69 @@
           this.dialogErrors = true;
           this.sectionFormat = true;
         }
-        }
       },
-      validateIPS(){
-        this.errors = [];
-        this.formats = [];
-        let required = ["identifier", "type", "timestamp", "entry"];
-        let ips;
-        try{
-            ips = JSON.parse(this.ips);
-            if (! this.isObject(ips)){
-              this.formats.push("ips no es un json");
-              this.dialogErrors = true;
-              this.sectionFormat = true;
-              return;
-            }
-        }
-        catch(e){
-          this.formats.push("ips no es un json");
-          this.dialogErrors = true;
-          this.sectionFormat = true;
-          return;
-        }
-        for( let k of required){
-          let v = ips[k];
-          if( v == undefined ){
-            this.errors.push(k);
+      validateCardAndFormat(fields, obj){
+        for( let k in fields){ //value: fields[k] 
+          let val = fields[k]; // [card, tipo de dato, {más variables}]
+          let card = val[0];
+          let v = obj[k]; //
+          if( v == undefined && (card == 0 || card == 1) ){ //no se encontró y era obligatorio:
+            this.cardErrors.push(k);
           }
-        }
-        if( ips.identifier != undefined && !this.isObject(ips.identifier)){
-          this.formats.push("identifier");
-        }
-        if((ips.entry != undefined && ! this.isArray(ips.entry)) || (ips.entry.length == 0)){
-          this.formats.push("entry");
-        }
-        else{ // if entry is valid i will check that each slices contains the fields fullUrl and resource
-          let countfullUrl = 0;
-          let countResource = 0;
-          for (let obj of ips.entry){
-            let f = obj['fullUrl'];
-            let r = obj['resource'];
-            if( f == undefined && countfullUrl == 0){
-              this.errors.push('fullUrl en una o más slice de entry');
-              countfullUrl = 1;
+          else if (v != undefined){ // todos los otros q se encontraron: reviso el tipo de dato y sus posibles variables internas
+            if (val[1] < 2){ //tenemos el tipo de dato conocido (0 o 1), ya que 2 es string o no conocido
+              let dataType = val[1];
+              // 0: obj,  1: array
+              if( (dataType == 0 && ! this.isObject(v)) || (dataType == 1 && ! this.isArray(v)) ){ //si debiese ser un obj y no es un obj... error de formato // lo mismo con array
+                this.formatErrors.push(k);
+              }
+              if (dataType == 1 && card == 1 && v.length<1){ //requiere ser más de 1 y el tipo de dato es array
+                this.cardErrors.push("elementos en " + k);
+              }
             }
-            if( r == undefined && countResource == 0){
-              this.errors.push('resource en una o más slice de entry');
-              countResource = 1;
-            }
-            if( countfullUrl == 1 && countResource == 1){
-              break;
+            //reviso los parametros internos si es q tiene
+            console.log("estoy en: ", k, "   ",  v," revisare los campos internos (3ra var): ", val)
+            if (val[2].length !=  0){
+              if (val[1] == 1){ // es un array, revisar por cada elemento del array q se cumplan las validaciones
+                for (let elem of v){
+                  this.validateCardAndFormat(val[2], elem);
+                }
+              }
+              else{
+                this.validateCardAndFormat(val[2], v);
+              }
             }
           }
         }
-        //this.validateComposition(ips);
-        if(this.errors.length > 0){
-          this.dialogErrors = true;
-          this.sectionErrors = true;
-        }
-        if(this.formats.length >0){
-          this.dialogErrors = true;
-          this.sectionFormat = true;
-        }
-        
       },
       validateComposition(ips){
-        let requiredOnce = ["status", "type", "type-coding-system", "type-coding-code", "subject", "subject-reference", "date", "title", ];
-        let requiredMore = ["type-coding", "author", "section"];
+        // 0: 1..1    //1: 1..*    //2: 0..1   //3: 0..*
+        // 0 and 1: required (only once) or (once or more).
+        // 2 and 3: optional 
+
+        //0: obj, 1:array, 2: string or idk
+        // si no es undefined y tiene 3ra variable... revisar los hijos... asi hasta el sgte, tipo arbol
+        let fields = {"status": [0, 2, {}], 
+        "type": [0, 0, {
+          "id": [2, 2, {}],
+          "extension": [3, 2, {}],
+          "coding": [1, 1, {
+            "id": [2, 2, {}],
+            "extension": [ 3, 2, {}], 
+            "system": [ 0, 2, {}],
+            "version": [ 2, 2, {}],
+            "code": [ 0, 2, {}],
+            "display": [ 2, 2, {}],
+            "userSelected": [ 2, 2, {}]
+          }],
+          "text": [2, 2, {}]
+        }], 
+        "subject": [0, 0, {
+
+        }], 
+        "date": [0, 2, {}], 
+        "title": [0, 2, {}]
+        }
         let resource;
         
         //check if the field resource field exists
@@ -243,115 +203,8 @@
           this.errors.push('Composition');
           return;
         }
-        for( let k of required){
-          let v = ips[k]
-          if( v == undefined){
-            this.errors.push(k);
-          }
-        }
-        let validate = false;
-        for( let obj of this.sampleJson.entry){
-          if (obj.resource.resourceType == 'Composition'){
-            let resource = obj.resource;
-            //checking required values
-            try{
-              let status = resource.status;
-            }
-            catch(e){
-              this.errors.push("status");
-            }
 
-            try{
-              let type = resource.type;
-              if( !this.isObject(type)){
-                this.errors.push("type");
-              }
-            }
-            catch(e){
-              this.errors.push("type");
-            }
-            try{
-              let coding = resource.type.coding;
-              if( !this.isArray(coding) ){
-                this.errors.push("type-coding");
-              }
-            }
-            catch(e){
-              this.errors.push("type-coding");
-            }
-            try{
-              let system = resource.type.coding[0].system;
-            }
-            catch(e){
-              this.errors.push("type-coding-system");
-            }
-            try{
-              let code = resource.type.coding[0].code;
-            }
-            catch(e){
-              this.errors.push("type-coding-code");
-            }
-
-            try{
-              let subject = resource.subject;
-              if( !this.isObject(subject)){
-                this.errors.push("subject");
-              }
-            }
-            catch(e){
-              this.errors.push("subject");
-            }
-            try{
-              let reference = resource.subject.reference;
-              if( typeof reference != "string"){
-                this.errors.push("subject-reference");
-              }
-            }
-            catch(e){
-              this.errors.push("subject-reference");
-            }
-
-            try{
-              let date = resource.date;
-            }
-            catch(e){
-              this.errors.push("date");
-            }
-
-            try{
-              let author = resource.author;
-              if( !this.isObject(author)){
-                this.errors.push("author");
-              }
-            }
-            catch(e){
-              this.errors.push("author");
-            }
-
-            try{
-              let title = resource.title;
-              if( typeof title != "string"){
-                this.errors.push("title");
-              }
-            }
-            catch(e){
-              this.errors.push("title");
-            }
-
-            try{
-              let section = resource.section;
-              if( ! this.isArray(section)){
-                this.errors.push("section");
-              }
-            }
-            catch(e){
-              this.errors.push("section");
-            }
-
-            //checking others possible values.
-            //if there are others values then the ips isn't valid
-          }
-        }
+        this.validateCardAndFormat(fields, resource);
       },
     },
     computed: {
